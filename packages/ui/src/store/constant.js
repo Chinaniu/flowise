4deecf8e991e1ff7c4c848b153eb5a622499c1b4
Chinaniu <477756621@qq.com>
2024-02-01 11:18:49 +0800

    sdsd

--- packages/ui/src/store/constant.js
+++ packages/ui/src/store/constant.js
@@ -3,18 +3,18 @@ export const gridSpacing = 3
 export const drawerWidth = 260
 export const appDrawerWidth = 320
 export const maxScroll = 100000
-export const baseURL = 'http://localhost:3100'
-// process.env.NODE_ENV === 'production' ? window.location.origin : window.location.origin.replace(':8080', ':3000')
+// export const baseURL = 'http://localhost:3100'
+export const baseURL = process.env.NODE_ENV === 'production' ? window.location.origin : window.location.origin.replace(':8080', ':3000')
 export const createdBy = document.cookie
     .split('; ')
-    .find((row) => row.startsWith('LOWCODER_CE_SELFHOST_TOKEN='))
+    .find((row) => row.startsWith('Flowise='))
     .split('=')[1]
     .split(':')[2]
 // export const createdBy = '65536f412886657aeb845dc9'
 // export const orgId = '65536f422886657aeb845dca'
 export const orgId = document.cookie
     .split('; ')
-    .find((row) => row.startsWith('LOWCODER_CE_SELFHOST_TOKEN='))
+    .find((row) => row.startsWith('Flowise='))
     .split('=')[1]
     .split(':')[4]
 
